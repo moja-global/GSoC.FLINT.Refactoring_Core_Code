c4627a3973ee1b16b74167b763993efd29c0eced
parth-gr <50455445+parth-gr@users.noreply.github.com>
2020-08-06 11:06:05 +0530

    Testing FMT and Fast Format against Boost Format

---
+++ moja-benchmark/fmt-7.0.1/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Development/moja-global/GSoC.FLINT.Refactoring_Core_Code/moja-benchmark/fmt-7.0.1
+# Build directory: E:/Development/moja-global/GSoC.FLINT.Refactoring_Core_Code/moja-benchmark/fmt-7.0.1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("test")
--- moja-benchmark/src/variant.cpp
+++ moja-benchmark/src/variant.cpp
@@ -1,8 +1,26 @@
 #include <Poco/Dynamic/Var.h>
 #include <benchmark/benchmark.h>
 
+//Variant
 #include <any>
 #include <variant>
+
+//FMT
+#include <fmt/core.h>
+
+//Fast Format
+//#include <fastformat/fastformat.hpp>
+//#include <fastformat/sinks/ostream.hpp>
+
+//Boost
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
+#include "boost/format.hpp"
+using namespace std;
+using boost::format;
+using boost::io::group;
 
 using namespace Poco::Dynamic;
 
@@ -18,5 +36,35 @@ static void BM_variantCreation(benchmark::State& state) {
   for (auto _ : state) std::variant<std::string, int> data("42");
 }
 BENCHMARK(BM_variantCreation);
+
+//FMT
+static void BM_FMT(benchmark::State& state) {
+    fmt::print("The answer is {}.", 42);
+}
+    BENCHMARK(BM_FMT);
+
+//Fast Format 
+    static void BM_Fast(benchmark::State& state) {
+        //std::string             sink;
+        //char const* arg0 = "arg0";
+        //std::string             arg1 = "arg1";
+        //int                     arg2 = 10;
+        //sink.erase();
+        //fastformat::fmt(sink, "A c-style string: {0}", arg0);
+        //sink.erase();
+        //fastformat::fmt(sink, "A std::string: {0}", arg1);
+        //sink.erase();
+        //fastformat::fmt(sink, "A c-style string: {0}, and a std::string: {1}", arg0, arg1);
+        //sink.erase();
+        //fastformat::fmt(sink, "A c-style string: {1}, and an int: {0}", arg2, arg0);
+    }
+    BENCHMARK(BM_Fast);
+    
+    
+    //Boost
+    static void BM_Boost(benchmark::State& state) {
+        cout << format("%1% %2% %3% %2% %1% \n") % "11" % "22" % "333";
+    }
+    BENCHMARK(BM_Boost);
 
 BENCHMARK_MAIN();
